30e02f8e683173e55f2b5b14d5f0ed2deeed04c7
Ilnur Amirov <iramirov@gmail.com>
2025-09-17 11:22:56 +0400

    переделалл алгоритм шифрования

--- 8-ctypto/script.js
+++ 8-ctypto/script.js
@@ -1,21 +1,29 @@
 function crypto(password) {
-    let arrPassword = password.split("");
-    arrPassword.reverse();
+    const step = 2;
+    let arrPassword = [];
+    for (let i = 0; i < password.length; i += step) {
+        arrPassword.push(password.slice(i, i + step));
+    }
+    for (let i = 0; i < arrPassword.length; i++) {
+        arrPassword[i] = arrPassword[i].split("").reverse().join("");
+    }
     return arrPassword.join("");
 }
 let originalPassword = "пароль";
 
 console.log(crypto(originalPassword));
 
-function decrypt(encryptedPassword, originalPassword) {
-    let arrPassword = encryptedPassword.split("");
-    arrPassword.reverse();
-    let reversPass = arrPassword.join("");
-    if (originalPassword === reversPass) {
-        return true;
-    } else {
-        return false;
-    }   
+function check(encryptedPassword, originalPassword) {
+   const step = 2;
+    let arrEncryptedPassword = [];
+    for (let i = 0; i < encryptedPassword.length; i += step) {
+        arrEncryptedPassword.push(encryptedPassword.slice(i, i + step));
+    }
+    for (let i = 0; i < arrEncryptedPassword.length; i++) {
+        arrEncryptedPassword[i] = arrEncryptedPassword[i].split("").reverse().join("");
+    }
+    return arrEncryptedPassword.join("") === originalPassword;
+   
 }
 
-console.log(decrypt("длороп", originalPassword));
+console.log(check("апорьл", originalPassword));
