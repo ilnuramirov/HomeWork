9bc20b8333fbc38887710b82034231df4b1455f5
Ilnur Amirov <iramirov@gmail.com>
2025-09-21 22:54:11 +0400

    3 2исправил

--- 11-date-array/script.js
+++ 11-date-array/script.js
@@ -1,36 +1,38 @@
 const dates = ['10-02-2022', 'тест', '11/12/2023', '00/13/2022', '41/12/2023'];
 
-let trueFormattedDates = dates.filter(function(date) {
-  if (date.includes('-')) {
-    return date.split('-');
-  } else if (date.includes('/')) {
-    return date.split('/');
-  } else {
-    return false;
-  }
-}).map(function(dateParts) {
-  let arrDateTemp;
-  let arrDate;
+let validDates = dates
+  .map(function(date) {
+    let separator = null;
+    if (date.includes('-')) {
+      separator = '-';
+    } else if (date.includes('/')) {
+      separator = '/';
+    } else {
+      return null;
+    }
 
-  if (dateParts.includes('-')) {
-    arrDate = dateParts.split('-');
-  } else if (dateParts.includes('/')) {
-    arrDate = dateParts.split('/');
-  }
-  return arrDate;
-});
-
-let validDates = [];
-for (let i = 0; i < trueFormattedDates.length; i++) {
-  let day = Number(trueFormattedDates[i][0]);
-  let month = Number(trueFormattedDates[i][1]);
-  let year = Number(trueFormattedDates[i][2]);
-
-  if (day > 0 && day < 32 && month > 0 && month < 13 && year > 0) {
-    const dayStr = String(day).padStart(2, '0');
+    const arrDate = date.split(separator);
+    if (
+      arrDate.length === 3 &&
+      arrDate[0].length === 2 &&
+      arrDate[1].length === 2 &&
+      arrDate[2].length === 4 &&
+      !isNaN(arrDate[0]) &&
+      !isNaN(arrDate[1]) &&
+      !isNaN(arrDate[2])
+    ) {
+      const day = Number(arrDate[0]);
+      const month = Number(arrDate[1]);
+      const year = Number(arrDate[2]);
+      if (day > 0 && day < 32 && month > 0 && month < 13 && year > 0) {
+        // Добавляем ведущие нули
+        const dayStr = String(day).padStart(2, '0');
         const monthStr = String(month).padStart(2, '0');
-    validDates.push(`${dayStr}-${monthStr}-${year}`);
-  }
-}
+        return `${dayStr}.${monthStr}.${year}`;
+      }
+    }
+    return null;
+  })
+  .filter(Boolean);
 
 console.log(validDates);
